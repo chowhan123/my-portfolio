ab229dd180f3e588153a5b65d9c9bd27f8b45f54
Santhosh Chauhan <139686578+chowhan123@users.noreply.github.com>
2025-07-26 15:45:57 +0530

    Update constants in index.jsx

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -34,6 +34,7 @@ export const PROJECTS = [
     description:
       "A personal portfolio website built with React and Tailwind CSS to showcase my skills, projects, and contact information.",
     image: projectImage1,
+    githubLink: "https://github.com/chowhan123/my-portfolio",
   },
   {
     id: 3,
