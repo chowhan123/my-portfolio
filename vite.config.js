2548d50dea730aee7642c8588aeb0fa69b620cf3
Santhosh Chauhan <139686578+chowhan123@users.noreply.github.com>
2025-07-26 15:35:19 +0530

    Allow Render host in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
+  preview: {
+    port: process.env.PORT || 5173,
     host: true,
-    port: Number(process.env.PORT) || 5173,
-    allowedHosts: ['my-portfolio-v2i1.onrender.com', 'localhost'], // add your Render domain and localhost for dev
+    allowedHosts: ['my-portfolio-8huu.onrender.com'],
+  },
+  server: {
+    port: process.env.PORT || 5173,
+    host: true,
   }
 });
+
